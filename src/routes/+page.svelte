b84c6b17a64aa1742e3681980b38661d377efe5c
Sch <junhg0211@gmail.com>
2026-08-11 12:34:22 +0900

    download as image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import * as htmlToImage from 'html-to-image';
+
 	let raw: string = $state('');
 
 	let lines: string[] = $derived(() => {
@@ -35,55 +37,83 @@
 	function caps(word: string) {
 		return !/[a-z]/.test(word) && /[A-Z]/.test(word);
 	}
+
+	async function downloadImage() {
+		const node = document.querySelector('.preview') as HTMLElement;
+
+		if (!node) return;
+
+		try {
+			const dataUrl = await htmlToImage.toPng(node, {
+				cacheBust: true,
+				backgroundColor: 'white'
+			});
+			const link = document.createElement('a');
+			link.download = 'image.png';
+			link.href = dataUrl;
+			link.click();
+		} catch (error) {
+			console.error('Error generating image:', error);
+		}
+	}
 </script>
 
 <div class="container-container">
 	<div class="container">
-		<table>
-			{#each lines() as line, i}
-				{#if i === 0 || i === lines().length - 1}
-					<tr>
-						<td colspan="999"><b>{line}</b></td>
-					</tr>
-				{:else}
-					<tr>
-						{#each line as word, j}
-							<td
-								class:hyphen={word[word.length - 1] === '-' ||
-									word[word.length - 1] === '.' ||
-									word[word.length - 1] === '=' ||
-									line[j - 1] === '-' ||
-									line[j - 1] === '.' ||
-									line[j - 1] === '='}
-								class:spaced-left={word[0] !== '-' &&
-									word[0] !== '=' &&
-									word[0] !== '.' &&
-									line[j - 1] &&
-									line[j - 1][line[j - 1].length - 1] !== '-' &&
-									line[j - 1][line[j - 1].length - 1] !== '.' &&
-									line[j - 1][line[j - 1].length - 1] !== '='}
-								class:spaced-right={word[word.length - 1] !== '-' &&
-									word[word.length - 1] !== '=' &&
-									word[word.length - 1] !== '.' &&
-									line[j + 1] &&
-									line[j + 1][0] !== '-' &&
-									line[j + 1][0] !== '=' &&
-									line[j + 1][0] !== '.'}
-							>
-								{#each word.split('.') as part, j}
-									<span class:caps={caps(part)}>
-										{part}{j === word.split('.').length - 1 ? '' : '.'}
-									</span>
-								{/each}
-							</td>
-						{/each}
-					</tr>
-				{/if}
-			{/each}
-		</table>
+		<div
+			class="preview"
+			style="display: flex; flex-direction: column; align-items: center; justify-content: center; gap: 8px;"
+		>
+			<table>
+				{#each lines() as line, i}
+					{#if i === 0 || i === lines().length - 1}
+						<tr>
+							<td colspan="999"><b>{line}</b></td>
+						</tr>
+					{:else}
+						<tr>
+							{#each line as word, j}
+								<td
+									class:hyphen={word[word.length - 1] === '-' ||
+										word[word.length - 1] === '.' ||
+										word[word.length - 1] === '=' ||
+										line[j - 1] === '-' ||
+										line[j - 1] === '.' ||
+										line[j - 1] === '='}
+									class:spaced-left={word[0] !== '-' &&
+										word[0] !== '=' &&
+										word[0] !== '.' &&
+										line[j - 1] &&
+										line[j - 1][line[j - 1].length - 1] !== '-' &&
+										line[j - 1][line[j - 1].length - 1] !== '.' &&
+										line[j - 1][line[j - 1].length - 1] !== '='}
+									class:spaced-right={word[word.length - 1] !== '-' &&
+										word[word.length - 1] !== '=' &&
+										word[word.length - 1] !== '.' &&
+										line[j + 1] &&
+										line[j + 1][0] !== '-' &&
+										line[j + 1][0] !== '=' &&
+										line[j + 1][0] !== '.'}
+								>
+									{#each word.split('.') as part, j}
+										<span class:caps={caps(part)}>
+											{part}{j === word.split('.').length - 1 ? '' : '.'}
+										</span>
+									{/each}
+								</td>
+							{/each}
+						</tr>
+					{/if}
+				{/each}
+			</table>
+			<div style="color: #888; font-size: 6px;">zasoque.org/glossing</div>
+		</div>
 	</div>
 	<div class="container">
 		<textarea class="raw" bind:value={raw}></textarea>
+	</div>
+	<div class="container">
+		<button onclick={downloadImage}>Download as Image</button>
 	</div>
 </div>
 
@@ -101,6 +131,7 @@
 		display: flex;
 		justify-content: center;
 		align-items: center;
+		flex-direction: column;
 	}
 
 	table {
@@ -131,5 +162,10 @@
 	.caps {
 		text-transform: lowercase;
 		font-variant-caps: all-small-caps;
+	}
+
+	.preview {
+		padding: 1em;
+		box-sizing: border-box;
 	}
 </style>
